9ce279d227c521a1b4d5b609c62ebaa9c0943415
Geovane Araújo <geovane-araujo@outlook.com>
2022-08-07 08:55:15 -0300

    Starter

---
+++ src/main/java/com/hubplataforma/hubplataformabackend/HubPlataformaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hubplataforma.hubplataformabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubPlataformaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubPlataformaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hubplataforma/hubplataformabackend/HubPlataformaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubplataforma.hubplataformabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubPlataformaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
